c096a262b628c2da23408e826283e9f349d7e9e6
Marina Gromova <krapiva87sstu@mail.ru>
2023-12-01 16:19:08 +0400

    14-6

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose');
-
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
-
+const helmet = require('helmet');
 const routes = require('./routes/index');
-
 const handleError = require('./middlewares/handleError');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(helmet());
+
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(limiter);
 
 app.use(express.json());
 
